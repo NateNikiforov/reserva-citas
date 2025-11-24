c8554d274f55c5c075b619bb4f2e358710fc2a8a
NateNikiforov <natenikiforov26@gmail.com>
2025-11-24 12:37:06 -0400

    Ultimo cambio

--- scrc/sistema/EstrategiaPrecio.java
+++ scrc/sistema/EstrategiaPrecio.java
@@ -1,10 +1,6 @@
 package sistema;
 
-/*
- * STRATEGY
- * Define un comportamiento intercambiable para calcular el precio final según
- * el tipo de paciente.
- */
+
 public interface EstrategiaPrecio {
     double calcular(double base);
 }
