0beae205dfaa5610d488f516b4da933880d752be
NateNikiforov <natenikiforov26@gmail.com>
2025-11-24 12:39:07 -0400

    Cambio final

--- scrc/sistema/UsuarioFactory.java
+++ scrc/sistema/UsuarioFactory.java
@@ -1,10 +1,5 @@
 package sistema;
 
-/* FACTORY METHOD
- * Esta clase permite crear objetos Usuario sin que la lógica principal 
- * conozca los detalles de cada tipo de usuario.
- * Facilita extender el sistema agregando otros tipos de usuarios.
- */
 public class UsuarioFactory {
 
     public static Usuario crearPaciente(String nombre) {
